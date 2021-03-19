893cf5a69f4b63e8cbf8b41ad67b0fcbdefda4ae
Anar Baydamirov <anar.baydamirov@accessbank.az>
2021-03-19 15:34:38 +0400

    Add project files.

---
+++ AspNetCore.SignalR/Controllers/MessageController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.SignalR.Controllers
+{
+    [ApiController]
+    [Route("api/message")]
+    public class MessageController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetCore.SignalR/Hubs/MessageHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.SignalR.Hubs
+{
+    public class MessageHub:Hub
+    {
+    }
+}
